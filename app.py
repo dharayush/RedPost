7b8b71b06f7588a6f36deed773c3358307352ba3
dharayush <79864877+dharayush@users.noreply.github.com>
2024-01-20 03:11:57 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# import requests module 
+import requests 
+  
+# Making a get request 
+response = requests.get('https://api.github.com/') 
+  
+# print response 
+print(response) 
+  
+# print links 
+print(response.links) 
