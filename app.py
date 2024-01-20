696e91a2a26cae3ee747da5e33cfe21dcae1e5c0
dharayush <79864877+dharayush@users.noreply.github.com>
2024-01-20 03:12:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # import requests module 
 import requests 
-  
+import streamlit as st  
 # Making a get request 
 response = requests.get('https://api.github.com/') 
   
 # print response 
-print(response) 
+st.write(response) 
   
 # print links 
-print(response.links) 
+st.write(response.links) 
